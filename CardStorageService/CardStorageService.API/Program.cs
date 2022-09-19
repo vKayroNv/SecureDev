cabab48e091b6fde095450a742a4fa5ccf5a1180
vKayroNv <36284611+vKayroNv@users.noreply.github.com>
2022-09-19 11:28:21 +0300

    Логирование

--- CardStorageService/CardStorageService.API/Program.cs
+++ CardStorageService/CardStorageService.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.HttpLogging;
+using NLog.Web;
+
 namespace CardStorageService.API
 {
     public class Program
@@ -6,16 +9,35 @@ namespace CardStorageService.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            #region Logging
+
+            builder.Services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.RequestHeaders.Add("Authorization");
+                logging.RequestHeaders.Add("X-Real-IP");
+                logging.RequestHeaders.Add("X-Forwarded-For");
+            });
+
+            builder.Host.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+
+            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+            #endregion
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -23,7 +45,6 @@ namespace CardStorageService.API
             }
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
